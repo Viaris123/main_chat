2b22a1b2bdfbe08d6017095daa188e71316f665f
Bubba_228 <mccap.tim@gmail.com>
2022-01-20 10:57:08 +0300

    - Add client app with user interface. Version 1.0!

--- client2.py
+++ client2.py
@@ -4,31 +4,47 @@ import socket
 import json
 import PySimpleGUI as sg
 
+M_LIST = list()
+sg.theme('DarkAmber')
+
 
 def incom_msg():
     while True:
         data = sock.recv(2048)
         msg_tuple = json.loads(data, encoding='utf-8')
-        msg_text = msg_tuple[1]
-        msg_adr = msg_tuple[0]
-        print(msg_text, ' from ', msg_adr)
+        # msg_text = msg_tuple[1]
+        # msg_adr = msg_tuple[0]
+        M_LIST.append(msg_tuple)
+        window['--LIST--'].update(M_LIST)
+        window['--LIST--'].set_vscroll_position(1.0)
 
 
-def client_pipeline():
-    while True:
-        thread1 = threading.Thread(target=incom_msg, daemon=True)
-        thread1.start()
+# def client_pipeline():
+#     while True:
+#         thread1 = threading.Thread(target=incom_msg, daemon=True)
+#         thread1.start()
+#
+#         sock.send(input("Enter message: ").encode('utf-8'))
 
-        sock.send(input("Enter message: ").encode('utf-8'))
 
-
-sg.theme('DarkAmber')
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('127.0.0.1', 8008))
 
-list1 = list()
-layout = [[sg.Listbox(list1, size=(30, 10))],
-          [sg.Input(size=(30, 2), do_not_clear=False)],
-          [sg.Button('SEND'), sg.Button('EXIT')]
+
+layout = [[sg.Listbox(M_LIST, size=(50, 10), key='--LIST--')],
+          [sg.Input(size=(50, 2), do_not_clear=False)],
+          [sg.Button('Send'), sg.Button('Exit')]
           ]
-window = sg.Window('Fake Chat', layout)
+window = sg.Window('Chat V1.0', layout)
+thread1 = threading.Thread(target=incom_msg, daemon=True)
+thread1.start()
+while True:
+    event, value = window.read()
+    if event == 'Exit' or event == sg.WINDOW_CLOSED:
+        break
+
+    if event == 'Send':
+        sock.send(value[0].encode('utf-8'))
+
+window.close()
+sock.close()
